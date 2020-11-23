6330a66c5a5a87707c6c94f8c0ba23c91f2f69ea
Abdul Faheem <72849238+areyouroot@users.noreply.github.com>
2020-11-23 00:25:50 -0800

    Add files via upload

---
+++ macchanger_v2.0.bash
@@ -0,0 +1,101 @@
+#!/bin/bash
+echo 
+echo do not re name this program or else it will not run
+echo 
+echo run it as sudo su
+echo
+sudo echo
+ #try bolck
+echo this mac adress changer is created by us
+cdi=$(pwd)
+echo
+echo $cdi
+echo
+echo enter the interface eg wlan0,eth0,lo,etc...
+echo
+read infa
+echo
+card=$(ifconfig $infa | grep ether)
+
+if [[ $? == 0 ]]
+    then
+    echo $card
+    echo 
+    echo enter the 6 point mac adress eg 00:11:22:33:44:55 same like this
+    echo
+    echo or to randamizer the macc address press R or r
+    echo
+    read add
+    echo
+    echo 
+    r="r"
+    R="R"
+    
+    if [[ "$add" == "$r" || "$add" == "$R" ]]
+        then
+        a=$((($RANDOM % 254)+1))
+        b=$((($RANDOM % 254)+1))
+        c=$((($RANDOM % 254)+1))
+        d=$((($RANDOM % 254)+1))
+        e=$((($RANDOM % 254)+1)) 
+        f=$((($RANDOM % 254)+1))
+        a=$(echo "obase=16;$a" | bc)
+        b=$(echo "obase=16;$b" | bc)
+        c=$(echo "obase=16;$c" | bc)
+        d=$(echo "obase=16;$d" | bc)
+        e=$(echo "obase=16;$e" | bc)
+        f=$(echo "obase=16;$f" | bc)
+        ifconfig $infa down
+        ifconfig $infa hw ether $a:$b:$c:$d:$e:$f
+        while [[ $? -ne '0' ]]
+            do
+            echo dont worry trying again
+            a=$((($RANDOM % 254)+1))
+            b=$((($RANDOM % 254)+1))
+            c=$((($RANDOM % 254)+1))
+            d=$((($RANDOM % 254)+1))
+            e=$((($RANDOM % 254)+1))
+            f=$((($RANDOM % 254)+1))
+            a=$(echo "obase=16;$a" | bc)
+            b=$(echo "obase=16;$b" | bc)
+            c=$(echo "obase=16;$c" | bc)
+            d=$(echo "obase=16;$d" | bc)
+            e=$(echo "obase=16;$e" | bc)
+            f=$(echo "obase=16;$f" | bc)
+            ifconfig $infa hw ether $a:$b:$c:$d:$e:$f 
+        done
+        echo
+        echo now ur mac is changed
+        echo
+        ifconfig $infa up
+        echo         mac add of $infa old mac add was 
+        echo $card 
+        echo         now new = $a:$b:$c:$d:$e:$f
+        echo
+        ifconfig $infa
+        echo
+    else
+        ifconfig $infa down
+        ifconfig $infa hw ether $add
+        if [[ $? != 0 ]]
+            then
+            echo you have enter an invalid option
+            echo
+            echo enter a valid option 
+            $cdi/mcc_asd.bash
+        fi
+        ifconfig $infa up
+        echo now ur mac add of $infa is $add 
+        echo
+        ifconfig $infa
+        echo
+       
+    fi
+else
+    
+    
+    echo you have enter an invalid option
+    echo
+    echo enter a valid option 
+    $cdi/mcc_asd.bash
+fi
